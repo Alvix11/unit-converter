6c3dbbc446dd60223fe56fdc09a500ce972d6044
Alvix <alvix.wick@gmail.com>
2025-08-24 19:19:35 -0400

    Edited the names of some variables and removed print statements that were used as tests.

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from .forms import LengthForm
+from .utils import length_conversions
 
 # Create your views here.
 def length_converter(request):
@@ -9,15 +10,11 @@ def length_converter(request):
         form = LengthForm(request.POST)
         if form.is_valid():
             value = form.cleaned_data["value"]
-            from_convert = form.cleaned_data["from_convert"]
-            to_convert = form.cleaned_data["to_convert"]
-            
-            print(value)
-            print(from_convert)
-            print(to_convert)
+            from_unit = form.cleaned_data["from_convert"]
+            to_unit = form.cleaned_data["to_convert"]
             
             return render(request, "length.html", {
-                "result": value,
+                "result": length_conversions(value, from_unit, to_unit),
                 'show_button': False,
                 }
             )
