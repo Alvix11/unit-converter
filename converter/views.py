168e77cc69b9a3365c673a0493c29112ca7d2a95
Alvix <alvix.wick@gmail.com>
2025-08-24 20:29:14 -0400

    Created the function for weight conversion, which works exactly the same as the length conversion function.

--- converter/views.py
+++ converter/views.py
@@ -1,34 +1,56 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from .forms import LengthForm
-from .utils import length_conversions
+from .forms import LengthForm, WeightForm
+from .utils import length_conversions, weight_conversions
 
 # Create your views here.
 def length_converter(request):
     
     if request.method == "POST":
         form = LengthForm(request.POST)
+        
         if form.is_valid():
             value = form.cleaned_data["value"]
             from_unit = form.cleaned_data["from_convert"]
             to_unit = form.cleaned_data["to_convert"]
             
-            return render(request, "length.html", {
-                "result": length_conversions(value, from_unit, to_unit),
+            return render(request, 'length.html', {
+                'result': length_conversions(value, from_unit, to_unit),
                 'show_button': False,
                 }
             )
     else:
         form = LengthForm()
         
-    return render(request, "length.html", {
-        "form": form,
+    return render(request, 'length.html', {
+        'form': form,
         'show_button': True
         }
     ) 
 
 def weight_converter(request):
-    return HttpResponse("Esta es la vista de weight_converter")
+    
+    if request.method == "POST":
+        form = WeightForm(request.POST)
+        
+        if form.is_valid():
+            value = form.cleaned_data["value"]
+            from_unit = form.cleaned_data["from_unit"]
+            to_unit = form.cleaned_data["to_unit"]
+            
+            return render(request, 'weight.html', {
+                'result': weight_conversions(value, from_unit, to_unit),
+                'show_button': False
+                }
+            )
+    else:
+        form = WeightForm()
+        
+    return render(request, 'weight.html', {
+        'form': form,
+        'show_button': True
+        }
+    )
 
 def temperature_converter(request):
     return HttpResponse("Esta es la vista de temperature_converter")
